144657acec5d9dcfc787486d325305e53b0f601e
Konrad Zaglewski <konrad.zaglewski@gmail.com>
2015-02-24 10:48:41 +0100

    Stworzenie kontrolerow i form odpowiedzialnych za zakup produktu

---
+++ src/SklepBundle/Form/PurchaseType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SklepBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PurchaseType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('products')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SklepBundle\Entity\Purchase'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sklepbundle_purchase';
+    }
+}
